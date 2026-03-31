bb09472fb07fd2c05829f567ec7b5a324d05a8fd
Eli Wood <75440887+ew20kt@users.noreply.github.com>
2026-03-30 23:14:44 -0400

    added machine viewing

--- backend/machines.php
+++ backend/machines.php
@@ -1,3 +1,35 @@
 <?php
 include("connect.php");
 
+try{
+    $stmt = $pdo->prepare("SELECT m.id AS machine_id, m.name, m.description, m.image, mr.start_date, mr.end_date FROM `machines` AS m LEFT JOIN `machine_rentals` AS mr ON m.id = mr.machine_id ORDER BY m.id");
+    $success = $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $machines = [];
+    foreach($data as $entry){
+        $id = $entry["machine_id"];
+
+        if(!isset($machines[$id])){
+            $machines[$id] = [
+                'name' => $entry['name'],
+                'image' => $entry['image'],
+                'description' => $entry['description'],
+                'rentals' => []
+            ];
+        }
+
+        if($entry["start_date"] != null && $entry["end_date"] != null){
+            array_push($machines[$id]['rentals'], [
+                'startDate' => $entry["start_date"],
+                'endDate' => $entry["end_date"]
+            ]);
+        }
+        
+    }
+
+    echo json_encode($machines);
+} catch(Exception $e){
+    http_response_code(500);
+    die("SQL query error");
+}
--- js/machines.js
+++ js/machines.js
@@ -7,9 +7,76 @@ window.addEventListener("load", ()=>{
         }
     })
     .then(data=>{
-        
+        console.log(data)
+        for(const machine of Object.values(data)){
+            createMachineTile(machine, container);
+        }
     })
     .catch(()=>{
-
+        createMachineTile({
+            image: "images/placeholder.png",
+            name: "Missing"
+        }, container);
     })
+
+
+
+
+
+    function createMachineTile(machine, parent){
+        const container = document.createElement("div")
+        container.classList.add("machine-container")
+        
+        const name = document.createElement("h3")
+        name.classList.add("machine-title")
+        name.textContent = machine.name
+        container.appendChild(name)
+
+        const description = document.createElement("p")
+        description.classList.add("machine-description")
+        description.textContent = machine.description
+        container.appendChild(description)
+
+        if(!machine.image){
+            machine.image = "images/placeholder.png"
+        }
+
+        const image = document.createElement("img")
+        image.classList.add("machine-image")
+        image.src = machine.image
+        container.appendChild(image)
+
+        if(machine.rentals && machine.rentals.length > 0){
+            const rentalContainer = document.createElement("div")
+            image.classList.add("machine-rental-container")
+            container.appendChild(rentalContainer)
+
+            const rentalTitle = document.createElement("h3")
+            rentalTitle.classList.add("machine-rental-title")
+            rentalTitle.textContent = "In Use"
+            rentalContainer.appendChild(rentalTitle)
+
+            for (const rental of machine.rentals){
+                const dateContainer = document.createElement("p")
+                dateContainer.classList.add("machine-rental-date-container")
+
+                const start = document.createElement("span")
+                start.classList.add("machine-rental-date")
+                start.textContent = rental.startDate
+                dateContainer.appendChild(start)
+
+                dateContainer.appendChild(document.createTextNode(" - "))
+
+                const end = document.createElement("span")
+                end.classList.add("machine-rental-date")
+                end.textContent = rental.endDate
+                dateContainer.appendChild(end)
+                
+                rentalContainer.appendChild(dateContainer)
+            }
+        }
+        
+
+        parent.appendChild(container)
+    }
 })
